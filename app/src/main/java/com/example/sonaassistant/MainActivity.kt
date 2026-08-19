c86dc6c421a97b5964ee590a1f3cb00b3ed5f008
Sona  Assistant <sunny767625@gmail.com>
2026-08-19 20:56:30 +0530

    Create MainActivity.kt

---
+++ app/src/main/java/com/example/sonaassistant/MainActivity.kt
@@ -0,0 +1,8 @@
+package com.example.sonaassistant
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
